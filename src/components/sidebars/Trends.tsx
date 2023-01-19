8ad697926b0d0cddf5fe9fadaf76ad461b236726
J0kk3 <8036138+J0kk3@users.noreply.github.com>
2023-01-19 23:07:26 +0100

    added json-data to trends.tsx

--- src/components/sidebars/Trends.tsx
+++ src/components/sidebars/Trends.tsx
@@ -1,5 +1,39 @@
 
 
+const trends =
+    [
+        {
+            category: "Sports",
+            content: "some team won something!",
+            glideCount: 300
+        },
+        {
+            category: "Finance",
+            content: "Stop using Bitcoin!",
+            glideCount: 200
+        },
+        {
+            category: "PC & Games",
+            content: "New game released!",
+            glideCount: 800
+        },
+        {
+            category: "Economy",
+            content: "Sad story, bro",
+            glideCount: 200
+        },
+        {
+            category: "Celebrities",
+            content: "Who cares?",
+            glideCount: 700
+        },
+        {
+            category: "Programming",
+            content: "Peter Jackson is not a programmer...probably",
+            glideCount: 1200
+        },
+    ];
+
 const TrendsSidebar = () =>
 {
     return (
@@ -61,6 +95,7 @@ const TrendsSidebar = () =>
                     <span class="text-gray-400 text-sm">10 000 glides</span>
                 </div>
             </div>
+            { JSON.stringify( trends ) }
         </div>
     );
 };
