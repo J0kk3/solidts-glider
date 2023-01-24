d4a6f6e9bce352e9e19552203afffeee72aeeff2
Joakim Sjöström <8036138+J0kk3@users.noreply.github.com>
2023-01-24 23:21:47 +0100

    sidebar-links-moved-to-links.tsx-output-w/-For_login/register-routing

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,4 +1,5 @@
 //solid
+import { A } from "@solidjs/router";
 import { Component } from "solid-js";
 
 const LoginScreen: Component = () =>
@@ -41,9 +42,9 @@ const LoginScreen: Component = () =>
                             </div>
                             <div class="text-sm text-gray-600 pb-4">
                                 No Account Yet?{ " " }
-                                <a class="hover:underline" href="#">
+                                <A class="underline" href="/register">
                                     Create a new account
-                                </a>
+                                </A>
                             </div>
                             <div class="flex-it py-2">
                                 <button
